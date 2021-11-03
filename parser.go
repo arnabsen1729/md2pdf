dac0cc0e9317d1262432f04ee48b961b21223b8d
Arnab Sen <arnabsen1729@gmail.com>
2021-11-03 12:05:25 +0530

    Implemented parsing of bold and italics

--- parser.go
+++ parser.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
@@ -19,10 +20,16 @@ const (
 	heading4
 	heading5
 	heading6
+	bold
+	italic
 )
 
 // HeadingRE regex source: https://github.com/Python-Markdown/markdown/blob/master/markdown/blockprocessors.py#L448
-const HeadingRE = `(?:^|\n)(?P<level>#{1,6})(?P<header>(?:\\.|[^\\])*?)#*(?:\n|$)`
+const (
+	HeadingRE = `(?:^|\n)(?P<level>#{1,6})(?P<header>(?:\\.|[^\\])*?)#*(?:\n|$)`
+	BoldRE    = `(\*{2}\S+?\*{2})`
+	ItalicRE  = `(\*{1}\S+?\*{1})`
+)
 
 type token struct {
 	style   int
@@ -31,7 +38,43 @@ type token struct {
 
 // mdParser will parse the text passed into markdown tokens.
 type mdParser struct {
-	tokens []*token
+	lines [][]*token
+}
+
+func newTokenDerived(content string, style int) *token {
+	content = strings.TrimSpace(content)
+
+	if strings.HasPrefix(content, "**") && strings.HasSuffix(content, "**") {
+		return &token{bold, content[2 : len(content)-2]}
+	} else if strings.HasPrefix(content, "*") && strings.HasSuffix(content, "*") {
+		return &token{italic, content[1 : len(content)-1]}
+	} else {
+		return &token{style, content}
+	}
+}
+
+func inlineParseAndAppend(style int, content string) []*token {
+	var line []*token
+
+	InlineRE := fmt.Sprintf("%s|%s", BoldRE, ItalicRE)
+	re := regexp.MustCompile(InlineRE)
+	groups := re.FindAllStringIndex(content, -1)
+	lastPos := 0
+
+	for _, group := range groups {
+		if group[0] > lastPos {
+			line = append(line, newTokenDerived(content[lastPos:group[0]], style))
+		}
+
+		line = append(line, newTokenDerived(content[group[0]:group[1]], style))
+		lastPos = group[1]
+	}
+
+	if lastPos < len(content) {
+		line = append(line, newTokenDerived(content[lastPos:], style))
+	}
+
+	return line
 }
 
 // newParser returns a pointer to an object of mdParser.
@@ -42,7 +85,10 @@ func newParser(input string) *mdParser {
 	lines := strings.Split(input, "\n")
 	for _, line := range lines {
 		// headings
+		var currTokens []*token
+
 		r := regexp.MustCompile(HeadingRE)
+
 		if r.MatchString(line) {
 			level := len(r.FindStringSubmatch(line)[1])
 			content := strings.TrimSpace(r.FindStringSubmatch(line)[2])
@@ -50,12 +96,14 @@ func newParser(input string) *mdParser {
 			* heading 1-6 has value 1-6 according to the enum declaration above.
 			* So, we can directly use the length of `#` to determine the heading level
 			 */
-			p.tokens = append(p.tokens, &token{level, content})
+			currTokens = append(currTokens, &token{level, content})
+			p.lines = append(p.lines, currTokens)
+
 			continue
 		}
-
 		// paragraph
-		p.tokens = append(p.tokens, &token{para, line})
+		currTokens = append(currTokens, inlineParseAndAppend(para, line)...)
+		p.lines = append(p.lines, currTokens)
 	}
 
 	return p
